47378d15e4606c8fd0e29ae57e2d9112c92a9bcb
Juan <juancot.pe@gmail.com>
2026-05-12 12:42:02 +0200

    Finalizada interaccion1

--- src/components/IdCard.jsx
+++ src/components/IdCard.jsx
@@ -1,5 +1,16 @@
-function IdCard(props) {
-    return (
-        <div    className="card"></div>
-    )
+function IdCard({ lastName, firstName, gender, height, birth, picture }) {
+  return (
+    <div className="id-card">
+      <img src={picture} alt={`${firstName} ${lastName}`} />
+      <div className="id-card-info">
+        <p><strong>First name:</strong> {firstName}</p>
+        <p><strong>Last name:</strong> {lastName}</p>
+        <p><strong>Gender:</strong> {gender}</p>
+        <p><strong>Height:</strong> {height / 100}m</p>
+        <p><strong>Birth:</strong> {birth.toDateString()}</p>
+      </div>
+    </div>
+  );
 }
+
+export default IdCard;
